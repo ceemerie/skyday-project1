9075f7a5d72a13b026d23f568f9ce5fd9f88f76a
ceemerie <78707851+ceemerie@users.noreply.github.com>
2021-03-16 22:35:53 -0500

    Fixed 'Find me' button to show hourly forecast update

--- src/app.js
+++ src/app.js
@@ -127,6 +127,9 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(displayWeatherCond);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentLocation(event) {
